256191b7c23c091f1b31af65b0999b906aa8dd6d
Oleksiy Masaltsev <a.o.masaltsev@gmail.com>
2022-08-31 00:14:49 +0300

    ArrayOne

---
+++ src/main/java/ua/masaltsev/leetcode/ArrayOne.java
@@ -0,0 +1,18 @@
+package ua.masaltsev.leetcode;
+
+public class ArrayOne {
+    public int removeDuplicates(int[] nums) {
+
+        int index = 1;
+        int number = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > number) {
+                nums[index] = nums[i];
+                number = nums[i];
+                index++;
+            }
+        }
+        return index;
+    }
+}
