959bae1bb7fd564587af716eaf5c95ddeaa6448f
Oleksiy Masaltsev <a.o.masaltsev@gmail.com>
2022-08-31 22:15:32 +0300

    ArrayTwo

---
+++ src/test/java/ua/masaltsev/leetcode/ArrayTwoTest.java
@@ -0,0 +1,15 @@
+package ua.masaltsev.leetcode;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArrayTwoTest {
+
+    @Test
+    void maxProfit() {
+        assertEquals(7, new ArrayTwo().maxProfit(new int[] {7, 1, 5, 3, 6, 4}));
+        assertEquals(4, new ArrayTwo().maxProfit(new int[] {1, 2, 3, 4, 5}));
+        assertEquals(0, new ArrayTwo().maxProfit(new int[] {7, 6, 4, 3, 1}));
+    }
+}
